2bf386caf70136063d94ca55a23dcbaab5a7aed5
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2022-05-01 14:14:40 +0700

    Create AssemblyInfo.cs

---
+++ NewNoteBlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Копирайтер+")]
+[assembly: AssemblyDescription("Текстовый редактор для копирайтеров. Особенностью являются возможности подсчета: строк, слов, символов с учетом и без учета пробелов.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ПИ-211")]
+[assembly: AssemblyProduct("Копирайтер+")]
+[assembly: AssemblyCopyright("ПИ-211 ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6e2077c8-afbb-4187-a7d1-99f6405ebe7a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
